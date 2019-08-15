7fa963179cf86a125b4f78067e697524f46cc951
victoriak94 <vkamila6@gmail.com>
2019-08-15 19:32:14 -0400

    Fix Todoitems capitalization bug

--- vue_crash_todolist/src/components/Todos.vue
+++ vue_crash_todolist/src/components/Todos.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div v-bind:key="todo.id" v-for="todo in todos">
-      <TodoItem v-bind: todo="todo" />
+      <TodoItem v-bind:todo="todo" />
     </div>
   </div>
 </template>
 
 <script>
-import TodoItem from "./TodoItem.vue";
+import TodoItem from "./Todoitem.vue";
 
 export default {
   name: "Todos",
